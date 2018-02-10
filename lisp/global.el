819b19533bdb8986b81952c1d4f6d1dd9ba7f2ae
Kévin Rauscher <kevin.rauscher@tomahna.fr>
2018-02-10 19:16:11 +0100

    setup backup file directory

--- lisp/global.el
+++ lisp/global.el
@@ -1,22 +1,21 @@
 ;; global variables
-(setq column-number-mode        t)
+(setq backup-by-copying         t)
+(setq backup-directory-alist    `(("." . "~/.cache/emacs/backup")))
 (setq confirm-kill-emacs        #'y-or-n-p) ;; confirm before exiting emacs (todo remove when bad habits are gone)
 (setq create-lockfiles          nil)
 (setq inhibit-startup-screen    t)
-(setq make-backup-files         nil)
 (setq scroll-error-top-bottom   t)
 (setq sentence-end-double-space nil)
 
 ;; buffer local variables
-(setq-default
- indent-tabs-mode nil
- tab-width 4
- c-basic-offset 4)
-
-;; global keybindings
-(global-unset-key (kbd "C-z"))
-
-(global-auto-revert-mode 1)
+(setq-default c-basic-offset   4)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width        4)
 
 (use-package magit)
 (use-package evil-magit)
+
+(global-auto-revert-mode 1)
+
+;; global keybindings
+(global-unset-key (kbd "C-z"))

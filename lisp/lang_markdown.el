28b22508cd05491b5beed1331d17eae4a8ac624f
Kévin Rauscher <kevin.rauscher@tomahna.fr>
2018-02-11 20:49:55 +0100

    add minor modes to markdown mode

--- lisp/lang_markdown.el
+++ lisp/lang_markdown.el
@@ -4,3 +4,13 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
+
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (diff-hl-mode)
+            (diff-hl-flydiff-mode         t)
+            (linum-mode                   t)
+            (rainbow-mode                 t)
+            (rainbow-delimiters-mode      t)
+            (show-paren-mode              t)
+            (smartparens-mode             t)))

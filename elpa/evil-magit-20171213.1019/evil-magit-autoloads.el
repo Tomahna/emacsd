b1042a16070edd28874aaa7fdd38bbe756bdccbc
Kévin Rauscher <kevin.rauscher-ext@seloger.com>
2018-03-15 17:00:29 +0100

    update packages

--- elpa/evil-magit-20171213.1019/evil-magit-autoloads.el
+++
@@ -1,36 +0,0 @@
-;;; evil-magit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "evil-magit" "../../../../../../.emacs.d/elpa/evil-magit-20171213.1019/evil-magit.el"
-;;;;;;  "f7bfd4294fd433cfe32608cf5616df7c")
-;;; Generated autoloads from ../../../../../../.emacs.d/elpa/evil-magit-20171213.1019/evil-magit.el
-
-(autoload 'evil-magit-init "evil-magit" "\
-This function completes the setup of evil-magit. It is called
-automatically when evil-magit is loaded. The only reason to use
-this function is if you've called `evil-magit-revert' and wish to
-go back to evil-magit behavior.
-
-\(fn)" t nil)
-
-(autoload 'evil-magit-revert "evil-magit" "\
-Revert changes by evil-magit that affect default evil+magit behavior.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/elpa/evil-magit-20171213.1019/evil-magit-autoloads.el"
-;;;;;;  "../../../../../../.emacs.d/elpa/evil-magit-20171213.1019/evil-magit.el")
-;;;;;;  (23198 50040 249326 584000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-magit-autoloads.el ends here

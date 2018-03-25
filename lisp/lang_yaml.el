d12c03fdf7ed9fb3d4304056bfb310cd68120646
Kévin Rauscher <kevin.rauscher@tomahna.fr>
2018-03-25 16:09:57 +0200

    improve yaml configuration

--- lisp/lang_yaml.el
+++ lisp/lang_yaml.el
@@ -1,10 +1,24 @@
 ;; Yaml
 (use-package yaml-mode
-  :demand)
-(require 'yaml-mode)
+  :diminish yaml-mode)
+(use-package ansible
+  :commands ansible
+  :diminish ansible)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
-;; Ansible
-(use-package ansible
-  :demand)
-(add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
+(defun configure-yaml ()
+  (interactive)
+
+  (setq highlight-symbol-enabled  t)
+  (setq show-trailing-whitespace  t)
+
+  (ansible                      t)
+  (company-mode                 t)
+  (diff-hl-mode                 t)
+  (diff-hl-flydiff-mode         t)
+  (linum-mode                   t)
+  (show-paren-mode              t)
+  (smartparens-mode             t)
+  (yas-minor-mode               t))
+
+(add-hook 'yaml-mode-hook 'configure-yaml)

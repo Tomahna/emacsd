437a71033d9038a3730e775d61c8802d96ea3d42
Kevin Rauscher <kevin.rauscher@tomahna.fr>
2018-05-10 23:05:30 +0200

    fix doom theme in daemon mode

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -7,6 +7,15 @@
   :config
   (load-theme 'doom-one t)
   (unless (file-directory-p "~/.local/share/fonts") (all-the-icons-install-fonts))) ;; Install icon fonts if not present
+(defun daemon-doom-init (&optional _frame)
+  (load-theme 'doom-one t))
+(defun daemon-doom-reload (frame)
+  (when (or (daemonp) (not (display-graphic-p)))
+    (with-selected-frame frame
+      (run-with-timer 0.1 nil #'daemon-doom-init))))
+(add-hook 'after-make-frame-functions #'daemon-doom-init)
+(add-hook 'after-make-frame-functions #'daemon-doom-reload)
+(daemon-doom-init)
 
 ;; Solaire Mode (Highlight current buffer)
 (use-package solaire-mode

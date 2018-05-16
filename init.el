a62ae46a7981a57c51ad06ac622b056e80ec783c
Kevin Rauscher <kevin.rauscher@tomahna.fr>
2018-05-16 20:27:29 +0200

    ignore class files in projectile

--- init.el
+++ init.el
@@ -78,11 +78,12 @@
   (persistent-scratch-setup-default))
 (use-package projectile            ;;Project Mode
   :init
-  (setq projectile-completion-system 'ivy)
-  (setq projectile-enable-caching    t)
-  (setq projectile-cache-file         "~/.cache/emacs/projectile.cache")
-  (setq projectile-mode-line         '(:eval (format " [%s]" (projectile-project-name))))
-  (setq projectile-use-git-grep      t))
+  (setq projectile-globally-ignored-files (list "TAGS" "*.class"))
+  (setq projectile-completion-system      'ivy)
+  (setq projectile-enable-caching         t)
+  (setq projectile-cache-file             "~/.cache/emacs/projectile.cache")
+  (setq projectile-mode-line              '(:eval (format " [%s]" (projectile-project-name))))
+  (setq projectile-use-git-grep           t))
 (use-package restclient
   :commands restclient-mode
   :diminish restclient-mode)

9ab5f5297b12f32aca15dd3874d20c1c77a3c5d9
Kevin Rauscher <kevin.rauscher@tomahna.fr>
2018-06-27 20:24:56 +0200

    properly set popup-kill-ring shortcut

--- lisp/minor_clipboard.el
+++ lisp/minor_clipboard.el
@@ -1,7 +1,9 @@
-;; Clipboard
+(require 'evil)
+
 (use-package clipmon)
 (use-package popup-kill-ring)
 (add-to-list 'after-init-hook 'clipmon-mode-start)
 
-(evil-define-key 'normal popup-kill-ring-keymap (kbd "C-p") 'popup-kill-ring)
-(evil-define-key 'visual popup-kill-ring-keymap (kbd "C-p") 'popup-kill-ring)
+(evil-define-key 'normal 'global (kbd "C-p") 'popup-kill-ring)
+(evil-define-key 'insert 'global (kbd "C-p") 'popup-kill-ring)
+(evil-define-key 'visual 'global (kbd "C-p") 'popup-kill-ring)

549f88417e351915188b89206ec63e485708dbdc
Kevin Rauscher <kevin.rauscher@tomahna.fr>
2018-08-17 12:29:51 +0200

    [nix] add company suggestions

--- lisp/mode_nix.el
+++ lisp/mode_nix.el
@@ -1,10 +1,16 @@
 ;;Nix
 (use-package nix-mode
   :demand)
+(use-package company-nixos-options
+  :demand)
 (require 'nix-mode)
+(require 'company-nixos-options)
+
 (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
 (add-hook 'nix-mode-hook
           (lambda ()
+            (add-to-list 'company-backends 'company-nixos-options)
+
             (company-mode            t)
             (diff-hl-mode            t)
             (diff-hl-flydiff-mode    t)

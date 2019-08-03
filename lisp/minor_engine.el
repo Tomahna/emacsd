835e875359c585fe55935948b0ad17dd46a1d555
Kevin Rauscher <kevin.rauscher@ingenico.com>
2019-08-03 18:04:56 +0200

    [engine-mode] add cats source to search

--- lisp/minor_engine.el
+++ lisp/minor_engine.el
@@ -4,5 +4,7 @@
 (require 'engine-mode)
 
 (defengine duckduckgo "https://duckduckgo.com/?q=%s")
+(defengine cats "https://github.com/typelevel/cats/search?q=%s")
 
+(evil-ex-define-cmd "cats" 'engine/search-cats)
 (evil-ex-define-cmd "ddg" 'engine/search-duckduckgo)
